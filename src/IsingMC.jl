b78fd38f2e13799888a6add7a5244c152f470aac
Lenz Fiedler <l.fiedler@hzdr.de>
2021-11-19 13:22:07 -0500

    Hello world!

---
+++ src/IsingMC.jl
@@ -0,0 +1,17 @@
+"""
+# module IsingMC.jl
+
+- Julia version: 1.4.1
+- Author: fiedlerl
+- Date: 2021-11-19
+
+# Examples
+
+```jldoctest
+test
+julia>
+```
+"""
+module IsingMC
+    println("Hello world!")
+end

ee77bb34161b422caf5d94f69e761c710f9f2ac4
Lenz Fiedler <l.fiedler@hzdr.de>
2021-12-01 22:06:16 +0100

    Fixed bug in example

--- examples/TemperatureComparison.jl
+++ examples/TemperatureComparison.jl
@@ -12,8 +12,8 @@ temperatures = range(0.5, 10.0, step=0.5)
 for temp in temperatures
     energies = []
     newSim = IsingMC.MCSimulation(temp/IsingMC.boltzmannConstant, 20, 4.0)
-    IsingMC.initialize(newSim, 5000, "negative")
-    energy = IsingMC.performSimulation(newSim)
+    IsingMC.initialize(newSim, "negative")
+    energy = IsingMC.performSimulation(newSim, 5000)
     append!(energies, energy)
     println(temp, " ", energy)
 end

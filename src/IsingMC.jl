57850f61dd8c2213e77c460573007c6b4ad04219
Lenz Fiedler <l.fiedler@hzdr.de>
2021-12-01 19:04:12 +0100

    Added comments

--- src/IsingMC.jl
+++ src/IsingMC.jl
@@ -7,39 +7,42 @@
 
 # Examples
 
-```jldoctest
-test
-julia>
-```
+See examples subfolder.
+
 """
 
 
 module IsingMC
-    # I don't want to deal with unitful constants right now
-    boltzmannConstant =  8.617333262e10-5
-    # Also, why not set it to one?
-#     boltzmannConstant = 1.0
-
     using Statistics
     using Plots
-    ####################
-    # Definitions
-    ####################
-    # Holds a single MC simulation.
-    # I realize latticeSize should never be negative, but setting it as UInt prints it as hexad
+
+    # Boltzmann constant in atomic units.
+    boltzmannConstant =  8.617333262e10-5
+
     mutable struct MCSimulation
        temperatureK::Float64
        latticeSize::UInt64
-       stepsToEvolve::UInt64
        interactionStrength::Float64
        lattice
     end
-    MCSimulation(temperatureK, latticeSize, stepsToEvolve, interactionStrength) =
-            MCSimulation(temperatureK, latticeSize, stepsToEvolve, interactionStrength,
+    """
+    Create a new MC simulation. The lattice array is created automatically.
+
+    - `temperatureK::Float64`: Temperature of the simulation.
+    - `latticeSize::UInt64`: Size of the lattice (in either direction, a quadratic lattice is
+                             assumed.
+    - `interactionStrength::Float64`: Strength of the spin interaction (for energy calculation)
+    """
+    MCSimulation(temperatureK::Float64, latticeSize::UInt64, interactionStrength::Float64) =
+            MCSimulation(temperatureK, latticeSize, interactionStrength,
                          zeros(Int64, latticeSize, latticeSize))
 
-    # Visualize the current state of a simulation.
-    # This is probably not the best way to do this.
+    """
+    Visualize the current state of a simulation. This is probably not the best way to do this,
+    but it works for now.
+
+    - `sim::MCSimulation`: The simulation to be visualized.
+    """
     function visualizeSim(sim::MCSimulation)
         plusx = []
         plusy = []
@@ -54,7 +57,6 @@ module IsingMC
                     append!(minusx, i)
                     append!(minusy, j)
                 end
-
             end
         end
         plot(plusx, plusy, seriestype = :scatter, label = "plus", markershape = :square,
@@ -65,9 +67,15 @@ module IsingMC
     end
 
 
-    # Initialize the lattice of a simulation.
-    # I know this is inefficient still.
-    function initialize(sim::MCSimulation, initType="random")
+    """
+    Initialize a simulation.
+
+    - `sim::MCSimulation`: The simulation to be initialized.
+    - `initType::string`: Type of initialization to be performed. Default "random", assgning
+                           spins at random. "positive" or "negative" initializes the lattice
+                           entirely with positive or negative spins, respectively.
+    """
+    function initialize(sim::MCSimulation, initType::string="random")
         for i in 1:sim.latticeSize
             for j in 1:sim.latticeSize
                 if initType == "random"
@@ -87,9 +95,17 @@ module IsingMC
         end
     end
 
-    function getLocalHamiltonian(sim::MCSimulation, pointX, pointY)
+    """
+    Calculate the local Hamiltonian around a lattice point.
+
+    - `sim::MCSimulation`: The simulation which provides the lattice.
+    - `pointX::Int`: Lattice point X coordinate.
+    - `pointY::Int`: Lattice point Y coordinate.
+    """
+    function getLocalHamiltonian(sim::MCSimulation, pointX::Int, pointY::Int)
        thisPoint = sim.lattice[pointX,pointY]
        localHamiltonian = 0.0
+       # We assume periodic boundary conditions.
         if pointX == 1
             localHamiltonian += sim.lattice[sim.latticeSize, pointY]*thisPoint
         else
@@ -113,13 +129,19 @@ module IsingMC
         return localHamiltonian
     end
 
-    # Time evolve one lattice.
-    function timeEvolve(sim::MCSimulation, printEnergies=false)
+    """
+    For a given simulation, perform the "time" evolution.
+
+    - `sim::MCSimulation`: The simulation for which the time evolution will be performed.
+    - `stepsToEvolve::UInt644`: Time steps this simulation is being evolved for.
+    - `printEnergies::bool`: If true, energies are printed during the simulation.
+    """
+    function timeEvolve(sim::MCSimulation, stepsToEvolve, printEnergies::bool=false)
         averagedEnergy = 0.0
         energy = energyFromLattice(sim)
         averagedEnergy = energy
         acceptedSteps = 1
-        for step in 1:sim.stepsToEvolve
+        for step in 1:stepsToEvolve
             # Determine which point in the lattice may be flipped.
             posToFlip = rand(0:((sim.latticeSize*sim.latticeSize)-1))
             xToFlip = posToFlip ÷ sim.latticeSize+1
@@ -127,7 +149,8 @@ module IsingMC
             sim.lattice[xToFlip, yToFlip] *= -1
 
             # Calculate change due to flip.
-            deltaE = -0.5*sim.interactionStrength*getLocalHamiltonian(sim, xToFlip, yToFlip)
+            newEnergy = energyFromLattice(sim)
+            deltaE = newEnergy-energy
 
             # Determine whether the change should be accepted.
             accepted = true
@@ -139,9 +162,9 @@ module IsingMC
                 end
             end
 
-            # If accepted, update the energy and
+            # If accepted, update the energy.
             if accepted == true
-                energy = energyFromLattice(sim)
+                energy = newEnergy
                 acceptedSteps += 1
                 averagedEnergy = ((averagedEnergy*(acceptedSteps-1))+energy)/acceptedSteps
                 if printEnergies == true
@@ -154,20 +177,21 @@ module IsingMC
         return averagedEnergy
     end
 
+    """
+    Calculate the current total energy for a given simulation.
+
+    # Arguments
+
+    - `sim::MCSimulation`: Simulation for which to calculate the current energy.
+
+    """
     function energyFromLattice(sim::MCSimulation)
         energy = 0.0
         for i in 1:sim.latticeSize
             for j in 1:sim.latticeSize
-                # We assume periodic boundary conditions.
                 energy += getLocalHamiltonian(sim, i, j)
             end
         end
         return -0.5*energy*sim.interactionStrength
     end
-
-
-    ####################
-    # Tests
-    ####################
-
 end
